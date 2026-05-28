b4af43cc6d2e021f1bec1bca5a3630fb5d7cad8b
Parth Padhiyar <parth.posimyth@gmail.com>
2026-05-28 10:35:23 +0530

    Security : Scope Better Templates Library admin script to elementor_library screen only
    WordPress 7.0 Compatibility

--- controls/ooohboi-better-templates-library.php
+++ controls/ooohboi-better-templates-library.php
@@ -59,7 +59,10 @@ class OoohBoi_Better_Templates_Library {
 		} );
 
 		// admin styles
-		add_action( 'admin_enqueue_scripts', function() {
+		add_action( 'admin_enqueue_scripts', function( $hook ) {
+
+			$screen = get_current_screen();
+			if ( ! $screen || 'elementor_library' !== $screen->post_type ) return;
 
 			wp_enqueue_script(
 				'ele-btl-admin-js',
